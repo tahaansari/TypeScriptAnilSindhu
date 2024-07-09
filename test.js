719e68ca74e0421f1f599475b5aa4bdbd625075a
Taha Ansari <taha.m.ansari@gmail.com>
2024-07-09 16:14:13 +0300

    init

---
+++ test.js
@@ -0,0 +1,15 @@
+// let x = 10;
+// console.log(x)
+// function addMe(a:number,b:number){
+//     return a+b;
+// }
+// console.log(addMe(10,10));
+// let data1=123;
+// const data2=123;
+var user = {
+    name: "taha",
+    age: 31,
+    state: "Maharashta",
+    opentowork: false
+};
+console.log(user.name);
---
+++ test.ts
@@ -0,0 +1,19 @@
+// let x = 10;
+// console.log(x)
+// function addMe(a:number,b:number){
+//     return a+b;
+// }
+
+// console.log(addMe(10,10));
+
+// let data1=123;
+// const data2=123;
+
+const user:{name:string,age:number,state:string,opentowork:boolean} = {
+    name:"taha",
+    age:31,
+    state:"Maharashta",
+    opentowork:false
+}
+
+console.log(user.name)
